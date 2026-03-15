feac94b510413f0d641eaf008d53b8914fdf1d81
andreyvkalugin <andrey.v.kalugin@gmail.com>
2026-03-15 11:44:11 +0300

    CRSK-418 add news loader with llm parsing

---
+++ loader/loader.py
@@ -0,0 +1,51 @@
+import json
+import os
+from typing import List
+
+from crawl4ai import AsyncWebCrawler, BrowserConfig, CrawlerRunConfig, CacheMode
+from custom_crawler.extraction_strategy import GigaExtractionStrategy
+from custom_agent.custom_giga_agent import CustomGigaChat
+from langchain_community.document_loaders import AsyncHtmlLoader
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
+
+from loader.prompt import EXTRACTION_PROMPT
+
+
+class Loader:
+    def __init__(self):
+        self._llm_cred = os.getenv("GIGACHAT_CREDENTIALS")
+        self._llm_model = "GigaChat"
+        self._llm = CustomGigaChat(
+            credentials=self._llm_cred,
+            model=self._llm_model,
+            verify_ssl_certs=False,
+            temperature=0.3,
+        )
+
+    def _obtain_news(self, url: str) -> List[str]:
+        """Fetch webpages and extract text content"""
+        for doc in AsyncHtmlLoader(url).load():
+            html = doc.page_content
+            prompt = EXTRACTION_PROMPT.format(html=html[:8000])
+            response = self._llm.invoke([HumanMessage(content=prompt)])
+            return [response.content]
+
+    async def _crawl_news(self, url: str) -> List[str]:
+        try:
+            async with AsyncWebCrawler(config=BrowserConfig(verbose=True)) as crawler:
+                raw_news = await crawler.arun(
+                    url=url,
+                    config=CrawlerRunConfig(
+                        word_count_threshold=1,
+                        extraction_strategy=GigaExtractionStrategy(),
+                        cache_mode=CacheMode.BYPASS,
+                    ),
+                )
+                return [
+                    news["text"]
+                    for news in json.loads(raw_news.extracted_content)
+                    if news["error"] == False
+                ]
+        except Exception as e:
+            print(f"В ходе выполнения анализа статьи: [ {url} ] возникла ошибка.", e)
+        return []
--- loader/news_loader.py
+++ loader/news_loader.py
@@ -5,34 +5,16 @@ from crawl4ai import AsyncWebCrawler, BrowserConfig, CrawlerRunConfig, CacheMode
 from custom_crawler.extraction_strategy import GigaExtractionStrategy
 from dao.articles.articles_repository import ArticlesRepository
 from dao.vector.vector_repository import VectorRepository
+from loader.loader import Loader
 
-class NewsLoader:
+class NewsLoader(Loader):
     def __init__(self):
+        super().__init__()
         self.vector_repository = VectorRepository()
         self.articles_repository = ArticlesRepository()
 
     async def load_news(self):
         for url in self.articles_repository.all_relevant():
-            news = await self._obtain_news(url)
+            news = self._obtain_news(url)
             print(f"сохраняемые новость в векторную БД: {url}")
             self.vector_repository.save(news, url)
-
-    async def _obtain_news(self, url: str) -> List[str]:
-        try:
-            async with AsyncWebCrawler(config=BrowserConfig(verbose=True)) as crawler:
-                raw_news = await crawler.arun(
-                    url=url,
-                    config=CrawlerRunConfig(
-                        word_count_threshold=1,
-                        extraction_strategy=GigaExtractionStrategy(),
-                        cache_mode=CacheMode.BYPASS,
-                    ),
-                )
-                return [
-                    news["text"]
-                    for news in json.loads(raw_news.extracted_content)
-                    if news["error"] == False
-                ]
-        except Exception as e:
-            print(f"В ходе выполнения анализа статьи: [ {url} ] возникла ошибка.", e)
-        return []
